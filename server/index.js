6974ae8ea4907faf2e0b27b529b49b7ba7c0cc52
Otávio Calaça <otaviocx@gmail.com>
2018-08-28 11:16:57 -0300

    Updating the index load and inject the services ...

--- server/index.js
+++ server/index.js
@@ -7,8 +7,30 @@ const jwt = require('express-jwt');
 const { createDatabase } = require('./database');
 const createGraphQLSchema = require('./graphql');
 
+const { AuthService } = require('./graphql/types/auth/AuthService');
+const { PhotoService } = require('./graphql/types/photo/PhotoService');
+const { UserService } = require('./graphql/types/user/UserService');
+const { JwtBlacklistService } = require('./graphql/types/auth/JwtBlacklistService');
+
+const { Repository } = require('./database/Repository');
+
 const DEFAULT_PORT = 3001;
 const DEFAULT_SECRET = 'totally-unguessable-jwt-secret';
+
+const isRevokedCallback = async (req, payload, done) => {
+    const isRevoked = await JwtBlacklistService.getInstance().isRevoked(payload);
+    done(null, isRevoked);
+}
+
+const createServices = (user) => { 
+    const userRepository = new Repository('user');
+    const photoRepository = new Repository('photo');
+    return {
+        userService: new UserService(userRepository),
+        photoService: new PhotoService(photoRepository, user),
+        authService: new AuthService(JwtBlacklistService.getInstance())
+    }
+}
 
 const createServer = async ({ secret = DEFAULT_SECRET }) => {
     const app = express();
@@ -17,10 +39,20 @@ const createServer = async ({ secret = DEFAULT_SECRET }) => {
     app.use(
         '/graphql',
         cors(),
-        jwt({ secret, credentialsRequired: false }),
+        jwt({ 
+            secret, 
+            credentialsRequired: false,
+            isRevoked: isRevokedCallback
+        }),
         bodyParser.json(),
         apolloUploadExpress(),
-        graphqlExpress(({ user }) => ({ schema, context: { user, db } })),
+        graphqlExpress(({ user }) => ({ 
+            schema, 
+            context: { 
+                user,
+                ...createServices(user) 
+            } 
+        })),
     );
     app.use(
         '/graphiql',
