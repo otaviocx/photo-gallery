42814770828de0280d5a1382199ccf78ca0bea7c
Otávio Calaça <otaviocx@gmail.com>
2018-08-29 10:59:35 -0300

    Add token into request headers

--- client/src/apolloClient.js
+++ client/src/apolloClient.js
@@ -1,8 +1,28 @@
 import React from 'react';
-import ApolloClient from 'apollo-boost';
+import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
+import { createUploadLink } from 'apollo-upload-client';
+import { setContext } from 'apollo-link-context';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
-const apolloClient = new ApolloClient({ uri: 'http://localhost:3001/graphql' });
+const httpLink = createUploadLink({ uri: 'http://localhost:3001/graphql' });
+
+const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem('token');
+    return {
+        headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : "",
+        }
+    }
+});
+
+const cache = new InMemoryCache();
+const apolloClient = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache
+});
+  
 
 export const withApolloClient = App => (
     <ApolloProvider client={apolloClient}>
