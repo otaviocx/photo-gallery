66187c23c5161aadcecfa4ea4f2d7209656b82e3
Otávio Calaça <otaviocx@gmail.com>
2018-08-29 10:57:13 -0300

    Adding jit to token and fixing some auth issues

--- server/graphql/types/auth/AuthService.js
+++ server/graphql/types/auth/AuthService.js
@@ -1,9 +1,9 @@
 const { AuthenticationError } = require('apollo-server');
-const crypto = require("crypto");
 const jwt = require('jsonwebtoken');
+const uuidv4 = require("uuid/v4");
 
-function createJwtToken({_id, username}, secret) {
-    return jwt.sign({_id, username}, secret);
+function createJwtToken({_id, name}, secret) {
+    return jwt.sign({_id, name}, secret, {jwtid: uuidv4()});
 }
 
 class AuthService {
@@ -16,18 +16,17 @@ class AuthService {
     async login({username, password}) {
         const user = await this.userService.findOneUser({name: username});
         if(!user) {
-            throw AuthenticationError("invalid username or password.");
+            throw new AuthenticationError("invalid username or password.");
         }
         if(user.password !== this.userService.getHash(password)) {
-            throw AuthenticationError("invalid username or password.");
+            throw new AuthenticationError("invalid username or password.");
         }
         return createJwtToken(user, this.secret);
     }
 
     async logout(token) {
-        return this.jwtBlacklistService.revoke(token).then(() => {
-            this.jwtBlacklistService.isRevoked(token);
-        })
+        this.jwtBlacklistService.revoke(token);
+        return this.jwtBlacklistService.isRevoked(token);
     }
 }
 
